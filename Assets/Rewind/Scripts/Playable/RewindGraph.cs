583ed04ab626a6173cca6bbcb0c97fdfb5e1bec8
Javier Sandoval <lopea5643@gmail.com>
2019-12-19 16:33:29 -0600

    initial commit

---
+++ Assets/Rewind/Scripts/Playable/RewindGraph.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+public class RewindGraph : MonoBehaviour
+{
+    //hold singleton variable 
+    static RewindGraph _main;
+    
+    //create a singleton for the graph
+    //there should be only one instance of the graph
+    public static RewindGraph Main
+    {
+        get
+        {
+            if(!_main)
+            {
+                _main = new GameObject("Graph").AddComponent<RewindGraph>();    
+            }
+            return _main;
+        }
+    }
+    void Start()
+    {
+        PlayableDirector g;
+        g.playableAsset.
+    }
+}
